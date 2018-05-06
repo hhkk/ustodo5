632e8aae9bd3f399bb812149379076a63396c96a
hhkk <hank.khang@gmail.com>
2018-05-06 13:30:44 -0400

    after grails generate-views todo.Todo

--- server/grails-app/domain/todo/Todo.groovy
+++ server/grails-app/domain/todo/Todo.groovy
@@ -3,6 +3,10 @@ package todo
 
 import grails.rest.*
 
+
+// my updates like
+// curl -i -H "Content-Type: application/json" --data '{"done": true}' -X PUT 0:8080/todos/32
+// still seem to work without @Resorce. maybe @Resource put down files -- hbk
 //@Resource(uri = '/todos', readOnly = false, formats = ['json', 'xml'])
 class Todo {
     String description
