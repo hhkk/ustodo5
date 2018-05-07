52d1f1f92b2cd6f39abfd74aeeea11848d78bde3
hhkk <hank.khang@gmail.com>
2018-05-07 19:48:32 -0400

    created todohk using ~/utd/180501CAngSecAngular/todo/client>ng g component todohk - not yet working: http://localhost:4200/#/todohkhk until I add {path: 'todohkhk', component: TodohkComponent} // added by hk to app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {RouterModule,Routes} from '@angular/router';
 import {IndexComponent} from "./index/index.component";
+import {TodohkComponent} from "./todohk/todohk.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'index', pathMatch: 'full'},
   {path: 'index', component: IndexComponent}
+//  ,
+//  {path: 'todohkhk', component: TodohkComponent} // added by hk
 ];
 
 @NgModule({
